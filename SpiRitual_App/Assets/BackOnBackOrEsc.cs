fe1e5861e61435929f81e803876f39970eac79af
Matt Bond <Matt Bond>
2016-01-31 15:30:55 +0200

    Added back button support. Fixed rendering bugs. Nexus 5 gets  weird streak, why? noone knows.

---
+++ SpiRitual_App/Assets/BackOnBackOrEsc.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackOnBackOrEsc : MonoBehaviour {
+    public string SceneToGoBackTo;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(SceneToGoBackTo);
+        }
+	}
+}
